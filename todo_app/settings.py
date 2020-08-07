4c36fedadd9bdd5cae3986a1e6b2f47ff6d40a50
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-08-08 02:59:00 +0530

    💚 now it looks much better

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo.apps.TodoConfig'
+    'todo.apps.TodoConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# default uses locally shipped version at 'fontawesome/css/font-awesome.min.css'
