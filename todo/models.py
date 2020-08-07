95956d2f596fdc60d806ef987f246e13ce96740b
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-08-07 12:48:15 +0530

    🐛 unique title

--- todo/models.py
+++ todo/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
-
